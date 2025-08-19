eaafd46c471a0d4fc8fd9430c67f7a33208c6b49
Jonathan Ferreira <jonathanmbferreira@outlook.com>
2025-08-19 14:04:26 -0300

    remove library

--- DADOS DEPUTADOS.R
+++ DADOS DEPUTADOS.R
@@ -1,20 +1,20 @@
 # Buscar informações sobre deputados
 
-library(httr2)
-
-deputados <- function(pagina){
-  params = list(
+deputados <- function(pagina) {
+  params <- list(
     pagina = pagina,
     dataInicio = "1990-07-13",
     ordem = "ASC",
     ordenarPor = "nome"
   )
   res <- httr::GET("https://dadosabertos.camara.leg.br/api/v2/deputados", query = params)
-  res_cont <- httr::content(res, as = "text") |> jsonlite::fromJSON() |> purrr::pluck('dados')
+  res_cont <- httr::content(res, as = "text") |>
+    jsonlite::fromJSON() |>
+    purrr::pluck("dados")
   return(res_cont)
 }
 
-a <- purrr::map_dfr(1:10, ~{
+a <- purrr::map_dfr(1:10, ~ {
   deputados(.x)
 })
 
